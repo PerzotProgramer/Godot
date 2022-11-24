ab78e7b1b90b4e965e089dc4ad733caf168db9ff
PerzotProgramer <bartoszperz2007@gmail.com>
2022-11-24 23:37:48 +0100

    Ulepszono fizykę

--- GodotTest/scripts/Player.gd
+++ GodotTest/scripts/Player.gd
@@ -1,26 +1,27 @@
 extends KinematicBody2D
 
-export var speed = 1000
+export var speed = 5
+
+const UPDIR = Vector2(0,-1)
+
 var motion = Vector2(0,0)
 var gravity = 1000
 func movement(delta: float) -> void:
 	
 	motion.x = 0
-	if motion.y < gravity:
-		motion.y += speed * delta
-	if is_on_wall():
+	motion.y += 20
+	if is_on_floor():
 		motion.y = 0
 	
 	if Input.is_action_pressed("right"):
-		motion.x = + speed * delta * 20
+		motion.x = + speed / delta
+		
 	if Input.is_action_pressed("left"):
-		motion.x = - speed * delta * 20
-	if Input.is_action_pressed("up") and is_on_wall():
-		motion.y -= speed * delta * 50
-	if Input.is_action_just_released("up") and motion.y < 0:
-		motion.y += speed * delta * 20
+		motion.x = - speed / delta
+	if Input.is_action_pressed("up") and is_on_floor():
+		motion.y -= speed * 2 / delta
 	
-	move_and_slide(motion)
+	move_and_slide(motion,UPDIR)
 
 func _ready() -> void:
 	set_physics_process(true)
